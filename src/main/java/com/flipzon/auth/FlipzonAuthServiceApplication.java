f278beb0cc5945bc11080c490b8f5f6062be4e52
praveen <praveenprem369@gmail.com>
2026-03-11 23:05:01 +0530

    Intial commit to flipzon-auth-service.

---
+++ src/main/java/com/flipzon/auth/FlipzonAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flipzon.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipzonAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipzonAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flipzon/auth/FlipzonAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipzon.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipzonAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
